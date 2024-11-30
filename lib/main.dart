bf8e8c1490aff2e4a9727e65603128485f540dd0
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2024-11-30 16:42:03 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:async/async.dart';
 
-void main() => runApp(TrafficSimulationApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(TrafficSimulationApp());
+}
 
 class TrafficSimulationApp extends StatelessWidget {
   @override
@@ -25,6 +35,7 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
   int greenTimer1 = 10;
 
   TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
   int redTimer2 = 13;
   int yellowTimer2 = 3;
   int greenTimer2 = 7;
@@ -39,6 +50,11 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
   late List<int> timers2;
   bool timerUpdated2 = false;
 
+  final ref1 = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/1/remaining_time");
+  final ref2 = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/2/remaining_time");
+  final ref3 = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/3/remaining_time");
+  final ref4 = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/4/remaining_time");
+
   @override
   void initState() {
     super.initState();
@@ -51,6 +67,8 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
     timers2 = [greenTimer2, yellowTimer2, redTimer2];
     currentTimer2 = timers2[currentIndex2];
     startTimer2();
+
+    fetchTrafficLight();
   }
 
   void startTimer1() {
@@ -87,25 +105,39 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
     });
   }
 
-  // void setTimer1(int red1, int yellow1, int green1) {
-  //   setState(() {
-  //     redTimer1 = red1;
-  //     yellowTimer1 = yellow1;
-  //     greenTimer1 = green1;
-  //     timerUpdated1 = true;
-  //   });
-  // }
-  //
-  // void setTimer2(int red2, int yellow2, int green2) {
-  //   setState(() {
-  //     redTimer2 = red2;
-  //     yellowTimer2 = yellow2;
-  //     greenTimer2 = green2;
-  //     timerUpdated2 = true;
-  //   });
-  // }
+  void nextCycle() {
+    setState(() {
+      if (currentIndex1 == 0) {
+        // Green to Yellow
+        currentIndex1 = 1;
+        currentTimer1 = yellowTimer1;
+      } else if (currentIndex1 == 1) {
+        // Yellow stays Yellow
+        currentIndex1 = 1;
+        currentTimer1 = yellowTimer1;
+      } else if (currentIndex1 == 2) {
+        // Red to 3 seconds Red
+        currentIndex1 = 2;
+        currentTimer1 = 3;
+      }
 
-  void setTimer(int red1, int green1) {
+      if (currentIndex2 == 0) {
+        // Green to Yellow
+        currentIndex2 = 1;
+        currentTimer2 = yellowTimer2;
+      } else if (currentIndex2 == 1) {
+        // Yellow stays Yellow
+        currentIndex2 = 1;
+        currentTimer2 = yellowTimer2;
+      } else if (currentIndex2 == 2) {
+        // Red to 3 seconds Red
+        currentIndex2 = 2;
+        currentTimer2 = 3;
+      }
+    });
+  }
+
+  void setTimer1(int green1, int red1) {
     setState(() {
       redTimer1 = red1;
       greenTimer1 = green1;
@@ -113,6 +145,17 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
       greenTimer2 = redTimer1 - 3;
       timerUpdated1 = true;
       timerUpdated2 = true;
+    });
+  }
+
+  void setTimer2(int green2, int red2) {
+    setState(() {
+      redTimer2 = red2;
+      greenTimer2 = green2;
+      redTimer1 = greenTimer2 + 3;
+      greenTimer1 = redTimer2 - 3;
+      timerUpdated2 = true;
+      timerUpdated1 = true;
     });
   }
 
@@ -134,9 +177,15 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
             left: 270,
             child: Column(
               children: [
-                Text("Đèn 1", style: TextStyle(color: Colors.white),),
+                const Text(
+                  "Đèn 1",
+                  style: TextStyle(color: Colors.white),
+                ),
                 buildTrafficLight(currentTimer1, currentIndex1),
-                Text("Đèn đỏ: $redTimer1 s, Đèn xanh: $greenTimer1 s", style: TextStyle(color: Colors.white),),
+                Text(
+                  "Đèn đỏ: $redTimer1 s, Đèn xanh: $greenTimer1 s",
+                  style: TextStyle(color: Colors.white),
+                ),
               ],
             ),
           ),
@@ -146,9 +195,15 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
             right: 270,
             child: Column(
               children: [
-                Text("Đèn 2", style: TextStyle(color: Colors.white),),
+                const Text(
+                  "Đèn 2",
+                  style: TextStyle(color: Colors.white),
+                ),
                 buildTrafficLight(currentTimer2, currentIndex2),
-                Text("Đèn đỏ: $redTimer2 s, Đèn xanh: $greenTimer2 s", style: TextStyle(color: Colors.white),),
+                Text(
+                  "Đèn đỏ: $redTimer2 s, Đèn xanh: $greenTimer2 s",
+                  style: TextStyle(color: Colors.white),
+                ),
               ],
             ),
           ),
@@ -157,9 +212,15 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
             right: 270,
             child: Column(
               children: [
-                Text("Đèn 3", style: TextStyle(color: Colors.white),),
+                const Text(
+                  "Đèn 3",
+                  style: TextStyle(color: Colors.white),
+                ),
                 buildTrafficLight(currentTimer1, currentIndex1),
-                Text("Đèn đỏ: $redTimer1 s, Đèn xanh: $greenTimer1 s", style: TextStyle(color: Colors.white),),
+                Text(
+                  "Đèn đỏ: $redTimer1 s, Đèn xanh: $greenTimer1 s",
+                  style: TextStyle(color: Colors.white),
+                ),
               ],
             ),
           ),
@@ -168,33 +229,71 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
             left: 270,
             child: Column(
               children: [
-                Text("Đèn 4", style: TextStyle(color: Colors.white),),
+                Text(
+                  "Đèn 4",
+                  style: TextStyle(color: Colors.white),
+                ),
                 buildTrafficLight(currentTimer2, currentIndex2),
-                Text("Đèn đỏ: $redTimer2 s, Đèn xanh: $greenTimer2 s", style: TextStyle(color: Colors.white),),
+                Text(
+                  "Đèn đỏ: $redTimer2 s, Đèn xanh: $greenTimer2 s",
+                  style: TextStyle(color: Colors.white),
+                ),
               ],
             ),
           ),
-
           Positioned(
-            top: 50,
-            left: 200,
-            child: SizedBox(
-              width: 300,
-              height: 50,
-              child: TextField(
-              decoration: InputDecoration(
-                labelText: "Nhập thời gian đèn đỏ và xanh",
-                hintText: "Ví dụ: 10 20",
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: () {
-                    List<String> values = (controller.text).split(" ");
-                    setTimer(int.parse(values[0]), int.parse(values[1]));
-                  },
-                ),
+            bottom: 10,
+            left: 10,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
               ),
-              controller: controller,
-                        ),
+              child: Column(
+                children: [
+                  const Text(
+                    "Bảng điều khiển",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Đèn 1 và 3",
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.check),
+                        onPressed: () {
+                          ref1.set(int.parse(controller.text));
+                          ref3.set(int.parse(controller.text));
+                        },
+                      ),
+                    ),
+                    controller: controller,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Đèn 2 và 4",
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.check),
+                        onPressed: () {
+                          ref4.set(int.parse(controller2.text));
+                          ref2.set(int.parse(controller2.text));
+                        },
+                      ),
+                    ),
+                    controller: controller2,
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                      onPressed: () {
+                        nextCycle();
+                        fetchTrafficLight();
+                      },
+                      child: Text("Chuyển đèn")),
+                  // Selection
+                ],
+              ),
             ),
           ),
         ],
@@ -216,7 +315,11 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
           SizedBox(
               height: 50,
               width: 70,
-              child: Center(child: Text('$currentTimer', style: TextStyle(fontSize: 30, color: Colors.white),))),
+              child: Center(
+                  child: Text(
+                '$currentTimer',
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ))),
         ],
       ),
     );
@@ -232,5 +335,21 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
         shape: BoxShape.circle,
       ),
     );
+  }
+
+  void fetchTrafficLight() {
+    ref1.onValue.listen((DatabaseEvent event) async {
+      int newTimer1 = event.snapshot.value as int;
+      DataSnapshot snapshot2 = await ref2.get();
+      int newTimer2 = snapshot2.value as int;
+      setTimer1(newTimer1, newTimer2);
+    });
+
+    ref2.onValue.listen((DatabaseEvent event) async {
+      int newTimer2 = event.snapshot.value as int;
+      DataSnapshot snapshot1 = await ref1.get();
+      int newTimer1 = snapshot1.value as int;
+      setTimer2(newTimer2, newTimer1);
+    });
   }
 }
