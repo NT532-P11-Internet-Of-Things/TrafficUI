7a1ef3ece00ac2985fab8fdacf14e165f20ee91a
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2024-12-01 15:42:52 +0700

    Lane count, auto update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:async/async.dart';
@@ -40,6 +41,9 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
   int yellowTimer2 = 3;
   int greenTimer2 = 7;
 
+  bool isAuto = true;
+
+  Offset controlPanelPosition = Offset(10, 10);
   late int currentTimer1;
   late int currentIndex1;
   late List<int> timers1;
@@ -54,6 +58,17 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
   final ref2 = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/2/green_time");
   final ref3 = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/3/green_time");
   final ref4 = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/4/green_time");
+
+  final lane1Ref = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/1/vehicle_count");
+  final lane2Ref = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/2/vehicle_count");
+  final lane3Ref = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/3/vehicle_count");
+  final lane4Ref = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/lanes/4/vehicle_count");
+
+  final autoRef = FirebaseDatabase.instance.ref("traffic_system/intersections/main_intersection/isAuto");
+  int lane1 = 0;
+  int lane2 = 0;
+  int lane3 = 0;
+  int lane4 = 0;
 
   @override
   void initState() {
@@ -171,129 +186,124 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
               fit: BoxFit.cover,
             ),
           ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                buildLane(1, lane1),
+                buildLane(3, lane3),
+              ],
+            ),
+          ),
+
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                buildLane(4, lane4),
+                buildLane(2, lane2),
+              ],
+            ),
+          ),
+
           // Đèn giao thông 1
-          Positioned(
-            top: 50,
-            left: 270,
+          Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                const Text(
-                  "Đèn 1",
-                  style: TextStyle(color: Colors.white),
-                ),
-                buildTrafficLight(currentTimer1, currentIndex1),
-                Text(
-                  "Đèn đỏ: $redTimer1 s, Đèn xanh: $greenTimer1 s",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-          // Đèn giao thông 2
-          Positioned(
-            top: 50,
-            right: 270,
-            child: Column(
-              children: [
-                const Text(
-                  "Đèn 2",
-                  style: TextStyle(color: Colors.white),
-                ),
-                buildTrafficLight(currentTimer2, currentIndex2),
-                Text(
-                  "Đèn đỏ: $redTimer2 s, Đèn xanh: $greenTimer2 s",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 50,
-            right: 270,
-            child: Column(
-              children: [
-                const Text(
-                  "Đèn 3",
-                  style: TextStyle(color: Colors.white),
-                ),
-                buildTrafficLight(currentTimer1, currentIndex1),
-                Text(
-                  "Đèn đỏ: $redTimer1 s, Đèn xanh: $greenTimer1 s",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 50,
-            left: 270,
-            child: Column(
-              children: [
-                Text(
-                  "Đèn 4",
-                  style: TextStyle(color: Colors.white),
-                ),
-                buildTrafficLight(currentTimer2, currentIndex2),
-                Text(
-                  "Đèn đỏ: $redTimer2 s, Đèn xanh: $greenTimer2 s",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 10,
-            left: 10,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  const Text(
-                    "Bảng điều khiển",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: "Đèn 1 và 3",
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.check),
-                        onPressed: () {
-                          ref1.set(int.parse(controller.text));
-                          ref3.set(int.parse(controller.text));
-                        },
-                      ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          "Đèn 1",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        buildTrafficLight(currentTimer1, currentIndex1),
+                        buildTimer(redTimer1, greenTimer1),
+                      ],
                     ),
-                    controller: controller,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: "Đèn 2 và 4",
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.check),
-                        onPressed: () {
-                          ref4.set(int.parse(controller2.text));
-                          ref2.set(int.parse(controller2.text));
-                        },
-                      ),
+                    // Center(
+                    //     child: Container(
+                    //       decoration: BoxDecoration(
+                    //         color: Colors.white,
+                    //         borderRadius: BorderRadius.circular(10),
+                    //       ),
+                    //       child: Column(
+                    //         children: [
+                    //           const Text(
+                    //             "Làn 1:",
+                    //             style: TextStyle(color: Colors.white),
+                    //           ),
+                    //           Text("Số xe: $lane1", style: TextStyle(color: Colors.black)),
+                    //         ],
+                    //       ),
+                    //     )),
+                    // Đèn giao thông 2
+                    Column(
+                      children: [
+                        const Text(
+                          "Đèn 2",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        buildTrafficLight(currentTimer2, currentIndex2),
+                        buildTimer(redTimer2, greenTimer2),
+                      ],
                     ),
-                    controller: controller2,
-                  ),
-                  const SizedBox(height: 10),
-                  ElevatedButton(
-                      onPressed: () {
-                        nextCycle();
-                        fetchTrafficLight();
-                      },
-                      child: Text("Chuyển đèn")),
-                  // Selection
-                ],
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          "Đèn 4",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        buildTrafficLight(currentTimer2, currentIndex2),
+                        buildTimer(redTimer2, greenTimer2),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          "Đèn 3",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        buildTrafficLight(currentTimer1, currentIndex1),
+                        buildTimer(redTimer1, greenTimer1),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            left: controlPanelPosition.dx,
+            top: controlPanelPosition.dy,
+            child: Draggable(
+              feedback: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
+              childWhenDragging: Container(
+                width: 200,
+                height: 200,
+              ),
+              onDragEnd: (details) {
+                setState(() {
+                  controlPanelPosition = details.offset;
+                });
+              },
+              child: buildControlPanel(),
             ),
           ),
         ],
@@ -351,5 +361,137 @@ class _TrafficSimulationScreenState extends State<TrafficSimulationScreen> {
       int newTimer1 = snapshot1.value as int;
       setTimer2(newTimer2, newTimer1);
     });
+
+    lane1Ref.onValue.listen((DatabaseEvent event) {
+      setState(() {
+        lane1 = event.snapshot.value as int;
+      });
+    });
+
+    lane2Ref.onValue.listen((DatabaseEvent event) {
+      setState(() {
+        lane2 = event.snapshot.value as int;
+      });
+    });
+
+    lane3Ref.onValue.listen((DatabaseEvent event) {
+      setState(() {
+        lane3 = event.snapshot.value as int;
+      });
+    });
+
+    lane4Ref.onValue.listen((DatabaseEvent event) {
+      setState(() {
+        lane4 = event.snapshot.value as int;
+      });
+    });
+
+    autoRef.onValue.listen((DatabaseEvent event) {
+
+        bool tempIsAuto = event.snapshot.value as bool;
+        if (tempIsAuto != isAuto) {
+          setState(() {
+            isAuto = tempIsAuto;
+          });
+        }
+    });
+  }
+
+  Widget buildControlPanel() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      width: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          const Text(
+            "Bảng điều khiển",
+            style: TextStyle(color: Colors.black),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "Đèn 1 và 3",
+              suffixIcon: IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () {
+                  ref1.set(int.parse(controller.text));
+                  ref3.set(int.parse(controller.text));
+                  setAuto(false);
+                },
+              ),
+            ),
+            controller: controller,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: "Đèn 2 và 4",
+              suffixIcon: IconButton(
+                icon: Icon(Icons.check),
+                onPressed: () {
+                  ref4.set(int.parse(controller2.text));
+                  ref2.set(int.parse(controller2.text));
+                  setAuto(false);
+                },
+              ),
+            ),
+            controller: controller2,
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text("Tự động"),
+              Switch(
+                  value: isAuto,
+                  onChanged: (value) {
+                setState(() {
+                  setAuto(value);
+                });
+              }),
+            ],
+          ),
+          ElevatedButton(
+              onPressed: () {
+                nextCycle();
+                fetchTrafficLight();
+              },
+              child: Text("Chuyển đèn")),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTimer(int redTimer1, int greenTimer1) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text("Đèn đỏ: $redTimer1 s, Đèn xanh: $greenTimer1 s",
+          style: TextStyle(color: Colors.black)),
+    );
+  }
+
+  Widget buildLane(int lane, int count) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text("Làn $lane:\nSố xe: $count", style: TextStyle(color: Colors.black)),
+    );
+  }
+
+  void setAuto(bool value) {
+    isAuto = value;
+    autoRef.set(value);
   }
 }
+
+
+
